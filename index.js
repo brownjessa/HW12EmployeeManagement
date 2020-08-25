fafaa524143e174c8c60080677f7f08dc43c4f0c
Jessamine Brown <brownjessa11@gmail.com>
2020-08-25 13:01:51 -0500

    display logo text, load prompts

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const db = require("./db");
 require("console.table");
 
 init();
+function init() {
+  const logoText = logo({ name: "Employee Manager" }).render();
+  console.log(logoText);
+  loadMainPrompts();
+}
