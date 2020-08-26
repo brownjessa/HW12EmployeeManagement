a1dc663bce8800733860d81d304d865e16ddd198
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 11:14:54 -0500

    fix password

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@ const util = require("util");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password:"Hurley2012#_!",
-    database: "employees"
+  host: "localhost",
+  user: "root",
+  password: "Marcie!1218",
+  database: "employees",
 });
 
 connection.connect();
